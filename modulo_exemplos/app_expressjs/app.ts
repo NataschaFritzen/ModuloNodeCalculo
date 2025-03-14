dbf34b5d12b82d0130e17e28ebcadcab4be9fe47
Natascha Fritzen <119437922+NataschaFritzen@users.noreply.github.com>
2025-03-14 11:48:36 -0300

    adiciona rotas principais ao servidor

--- modulo_exemplos/app_expressjs/app.ts
+++ modulo_exemplos/app_expressjs/app.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import clientsRouter from "./Router/Clients";
 
-const app = express();
+const app= express();
+
+app.use(clientsRouter);
 
 app.listen(3000, () =>{
 console.log("Servidor criado...")
